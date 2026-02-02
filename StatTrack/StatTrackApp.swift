847401e72642c9e673b545d6202d00edfbbcd187
Parker <Parker.engelson09@gmail.com>
2026-02-01 21:08:36 -0800

    Initial Commit

---
+++ StatTrack/StatTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatTrackApp.swift
+//  StatTrack
+//
+//  Created by Parker Engelson on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct StatTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
