b5bdfebc07f70b206049ed7fa4f04d0a7dea9ead
Parker <Parker.engelson09@gmail.com>
2026-02-01 21:25:03 -0800

    Custom button

--- StatTrack/ContentView.swift
+++ StatTrack/ContentView.swift
@@ -8,17 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+    @State private var points = 0
+    @State private var shots = 0
+    @State private var rebounds = 0
+    @State private var assists = 0
+    @State private var steals = 0
+    @State private var blocks = 0
+    @State private var turnovers = 0
+    @State private var fouls = 0
+    
+    @State private var shootingPercent = 0.0
+   var body: some View {
+        CounterButton()
+        
     }
 }
 
+struct CounterButton: View {
+    @State private var count = 0
+
+    var body: some View {
+        Button(action: {
+            count += 1
+        }) {
+            Text("\(count)")
+                .font(.largeTitle)
+                .padding()
+        }
+        .onLongPressGesture {
+            count = 0
+        }
+    }
+}
 #Preview {
     ContentView()
 }
